49acd4c2dd055d852f550d531c95063cdf55c6fa
Imran Shaikh <71130384+Imransh1@users.noreply.github.com>
2022-07-20 13:58:48 +0530

    Initial Commit

---
+++ BankCode/footer.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+<div class="container">
+<div class="follow_us">
+    <label>Follow Us</label>
+    <ul>
+        <li><a target="_blank" href="https://www.facebook.com/Indalacampus/">Facebook</a></li>
+        <li><a target="_blank" href="https://twitter.com/indalacampus">Twitter</a></li>
+        <li><a target="_blank" href="https://www.instagram.com/indalagroupofinstitutions">Instagram</a></li>
+        <li><a target="_blank" href="https://www.linkedin.com/">LinkedIn </a></li>
+    
+    </ul>
+    </div>
+    <div class="contact" id="contactus">
+        <label>Contact Us</label>
+        <ul>
+            <p><a target="_blank" href="http://icoe.ac.in/" >Indala College of Engineering,</a></p><p>Bapsai, Kalyan-Murbad Rd.</p>
+            <p>Tel : 1234567890</p>
+            <p> Email: <a target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=imran.shaikh@icoe.ac.in&Subject=Hey%20Imran"  >imran.shaikh@icoe.ac.in</a></p>
+            
+        </ul>
+    </div>
+        <div class="links">
+            <label>Important Links</label>
+            <ul>    
+                <li><a target=”_blank” href="https://google.com">Google</a></li>
+                <li><a target=”_blank” href="https://youtube.com">Youtube</a></li> 
+                <li><a href="https://reddit.com" target="_blank">Reddit</a></li>
+            <li><a target=”_blank” href="https://discord.com">Discord</a></li>
+        </ul><br>
+    </div>
+
+    <div class="links">
+            <label>Profiles</label>
+            <ul>    
+            <li><a target="_blank" href="https://twitter.com/mr__sh1">Imran Shaikh</a></li>
+            <li><a target="_blank" href="https://www.instagram.com/afaq_sk_">Afaq Shaikh</a></li>
+            <li><a target="_blank" href="https://www.instagram.com/mr_d3vil_op/">Sahil Shaikh</a></li>
+            <li><a href="">Tamish Trakroo</a></li>
+        </ul><br>
+    </div>
+    
+    
+    </div>
+    <div class="copyright">
+        <span>Copyright &copy; 2022-23 New Horizon Bank. All rights reserved by project team.</span>
+    </div>
+
+</footer>
---
+++ BankCode/header.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('log_errors','On');
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+?>
+<link rel="stylesheet" type="text/css" href="css/header.css">
+        <div class="header_main">
+		<div class="navBar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+                <li><a href="customer_login.php">Customer Login</a></li>
+				<li><a href="staff_login.php">Staff Login</a></li>
+				<li><a href="index.php#aboutus">About Us</a></li>
+			
+			</ul>
+		</div>
+	 <a href="index.php"><div class="logo-name">
+			<div class="logo">
+             <img class="logo_img" src="img/chase.jpg">
+			</div>
+			
+			<div class="name">
+				<h5> Online Banking System</h5></a><br>
+				<h6>In PHP</h6>
+			</div>
+        </div>
+            
+           
+            
+           
+			
+	</div>
